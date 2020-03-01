84f1480f05033ef3fc2b578d7c1e6bdf524e8cb1
james5101 <james.c.noonan@gmail.com>
2020-03-01 14:15:57 -0500

    Refactor code to resuable functions

--- create.py
+++ create.py
@@ -9,99 +9,100 @@ headers = {
   'X-Api-Key': (os.environ.get('HARNESS_API_KEY'))
 }
 
-def run_query(query, variables):
-    request = requests.post(url, json={'query': query, 'variables':variables}, headers=headers)
-    if request.status_code == 200:
-        return request.json()
-    else:
-        raise Exception("Query failed to run by returning code of {}. {}".format(request.status_code, query))
+def run_query_with_vars(query, variables):
+  request = requests.post(url, json={'query': query, 'variables':variables}, headers=headers)
+  if request.status_code == 200:
+    return request.json()
+  else:
+    raise Exception("Query failed to run by returning code of {}. {}".format(request.status_code, query))
 
-def run_query1(query1, variables1):
-    request = requests.post(url, json={'query': query1, 'variables':variables1}, headers=headers)
-    if request.status_code == 200:
-        return request.json()
-    else:
-        raise Exception("Query failed to run by returning code of {}. {}".format(request.status_code, query))
+def run_query_without_vars(query):
+  request = requests.post(url, json={'query': query}, headers=headers)
+  if request.status_code == 200:
+    return request.json()
+  else:
+    raise Exception("Query failed to run by returning code of {}. {}".format(request.status_code, query))
 
-def run_query2(query2):
-    request = requests.post(url, json={'query': query2}, headers=headers)
-    if request.status_code == 200:
-        return request.json()
-    else:
-        raise Exception("Query failed to run by returning code of {}. {}".format(request.status_code, query))
-
-             
-query = """
-mutation createapp($app: CreateApplicationInput!) {
-  createApplication(input: $app) {
-    clientMutationId
-    application {
-      name
-      id
-    }
-  }
-}
-
-"""
-variables = {
-  "app": {
-    "clientMutationId": "req9",
-    "name": "create_from_api",
-    "description": "test create harness application from api"
-  }
-}
-
-query1 = """
-mutation updateGitConfig($gitConfig: UpdateApplicationGitSyncConfigInput!) {
-  updateApplicationGitSyncConfig(input: $gitConfig) {
-    clientMutationId
-    gitSyncConfig {
-      branch
-      syncEnabled
-      gitConnector {
-        id
-        name
-        description
-        createdAt
-        createdBy {
+def get_git_connector_by_id(name):
+  query = """
+    {
+      connectors(
+        filters: [
+          { connectorType: { operator: EQUALS, values: GIT } }
+        ]
+        limit: 10
+      ) {
+        pageInfo {
+          total
+        }
+        nodes {
           id
           name
         }
       }
     }
-  }
-}
+    """
+  result = run_query_without_vars(query)
+  print(result)
 
-"""
-variables1 = {
-  "gitConfig": {
-    "clientMutationId": "req321",
-    "applicationId": "zHvqEutEQ6KcnoG55m8tOg",
-    "gitConnectorId": "_pRVp-R_TGqoIC0A60Y7gw",
-    "branch": "master",
-    "syncEnabled": "true"
-  }
-}
-
-query2 = """
-{
-  connectors(
-    filters: [
-      { connectorType: { operator: EQUALS, values: GIT } }
-    ]
-    limit: 10
-  ) {
-    pageInfo {
-      total
+def create_new_application(name, description):
+  query = """
+  mutation createapp($app: CreateApplicationInput!) {
+      createApplication(input: $app) {
+        clientMutationId
+        application {
+          name
+          id
+        }
+      }
     }
-    nodes {
-      id
-      name
+
+    """
+  variables = {
+      "app": {
+        "clientMutationId": "req9",
+        "name": name,
+        "description": description
     }
   }
-}
-"""
-# result = run_query(query, variables) 
-# result1 = run_query(query1,variables1)
-result2 = run_query2(query2)
-print(result2)
+  result = run_query_with_vars(query, variables)
+  print(result)
+
+def create_git_sync_with_app(name):
+  query = """
+  mutation updateGitConfig($gitConfig: UpdateApplicationGitSyncConfigInput!) {
+    updateApplicationGitSyncConfig(input: $gitConfig) {
+      clientMutationId
+      gitSyncConfig {
+        branch
+        syncEnabled
+        gitConnector {
+          id
+          name
+          description
+          createdAt
+          createdBy {
+            id
+            name
+          }
+        }
+      }
+    }
+  }
+
+  """
+  variables = {
+    "gitConfig": {
+      "clientMutationId": "req321",
+      "applicationId": "zHvqEutEQ6KcnoG55m8tOg",
+      "gitConnectorId": "_pRVp-R_TGqoIC0A60Y7gw",
+      "branch": "master",
+      "syncEnabled": "true"
+    }
+  }
+  result = run_query_with_vars(query, variables)
+  print(result)
+# get_git_connector_by_id("test")
+# create_new_application("name")
+# create_git_sync_with_app("name")
+create_new_application("test_create", "description")
